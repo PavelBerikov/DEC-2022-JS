b132b58147ef825fc7d67d8dfc1695cc70d1166d
PavelBerikov <NoooB199505@gmail.com>
2023-01-31 22:01:41 +0200

    DZ

---
+++ main1.js
@@ -0,0 +1,33 @@
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+console.log(a, b, c, d)
+{
+    let a = 1;
+    let b = 10;
+    let c = -999;
+    let d = 123;
+    let e = 3.14;
+    const PI = 3.14;
+    let f = 2.7;
+    let g = 16;
+    console.log(a, b, c, d, e, PI, f, g)
+    console.log(9 > 10);
+    console.log(9 <10);
+}
+let firstName = `Pavlo`;
+let middleName = `Sergiyovich`;
+let lastName = `Sidukov`;
+let fml = `${firstName} ${middleName} ${lastName}`;
+console.log(fml);
+{
+    let a = 100;
+    let b = '100';
+    let c = true;
+    console.log(typeof a, typeof b, typeof c);
+}
+let name = prompt('name');
+let pname = prompt('middleName');
+let age = prompt('age');
+console.log(name, pname, age);
