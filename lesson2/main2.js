016004ee57aa913fdb7cd8a1f29071c3fe946297
PavelBerikov <NoooB199505@gmail.com>
2023-02-03 21:59:08 +0200

    DZ

---
+++ lesson2/main2.js
@@ -0,0 +1,36 @@
+/*
+- Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+*/
+{
+    let a = [15, 'privet', 9 < 10, 293, 'red', 0 == 1, 'green', 89, 91, '123']
+    console.log(a[0])
+    console.log(a[1])
+    console.log(a[2])
+    console.log(a[3])
+    console.log(a[4])
+    console.log(a[5])
+    console.log(a[6])
+    console.log(a[7])
+    console.log(a[8])
+    console.log(a[9])
+}
+/*- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.*/
+{
+    let book1 = {
+        title: 'last wish',
+        pageCount: 500,
+        genre: 'fantasy'
+    }
+    let book2 = {
+        title: 'Sword of destiny',
+        pageCount: 450,
+        genre: 'fantasy'
+    }
+    let book3 = {
+        title: 'Elven blood',
+        pageCount: 650,
+        genre: 'fantasy'
+    }
+    console.log(book1, book2, book3)
+}
+/*- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - являється  масивом. Кожен автор має поля name та age.*/
---
+++ test/maintest.js
@@ -0,0 +1,54 @@
+/*let a = 20;
+let b = 18;
+if (a > b) {
+    console.log('a bolshe b')
+}else {
+    console.log('a menshe b')
+}*/
+/*let age = prompt('enter yor age');
+if (age >= 18){
+    console.log('hello')
+}else {
+    console.log('stop')
+}*/
+/*
+let a = +prompt('num')
+if (a == 1) {
+    console.log('ravno 1')
+} else if (a == 2) {
+    console.log('ravno 2')
+}else if (a == 3) {
+    console.log('ravno 3')
+}else {
+    console.log('drugoe')
+}*/
+/*
+let a = prompt('chislo');
+switch (a) {
+    case 'green':
+        console.log('go');
+        break;
+    case 'red':
+        console.log('stop');
+        break
+    default:
+        console.log('ffffffff')
+}*/
+let book1 = {
+    title: 'One blade',
+    pageCount: 500,
+    genre: 'sport',
+    authors:['sagsa','dads']
+
+}
+let book2 = {
+    title: 'Sword of destiny',
+    pageCount: 450,
+    genre: 'fantasy'
+}
+let book3 = {
+    title: 'Elven blood',
+    pageCount: 650,
+    genre: 'fantasy'
+}
+console.log(book1, book2, book3)
